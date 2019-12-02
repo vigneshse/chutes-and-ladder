4102ab5aec9206c0c3b45100c2534a07236c5462
Vignesh Selvaraj <vigneshse@gmail.com>
2019-12-01 18:03:41 -0800

    Added for 4 players

--- src/main/java/com/game/assignment/ChutesAndLadders.java
+++ src/main/java/com/game/assignment/ChutesAndLadders.java
@@ -3,8 +3,16 @@ package com.game.assignment;
 public class ChutesAndLadders {
 
     public static void main(String args[]) {
+
+        // Two Players
+        String[] players = {"Eric", "Paul"};
+        Game game = new Game(players);
+        game.play();
+
+/*      Four Players
         String[] players = {"Eric", "Paul", "Katy", "Carrie"};
         Game game = new Game(players);
         game.play();
+*/
     }
 }
